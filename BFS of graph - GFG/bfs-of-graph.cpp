25093ac19833f89592745aff6ce3bc632d5c9a3e
Ankit Kumar <61880496+anktsverma@users.noreply.github.com>
2022-02-14 09:12:51 +0530

    Added solution - LeetHub

--- BFS of graph - GFG/bfs-of-graph.cpp
+++ BFS of graph - GFG/bfs-of-graph.cpp
@@ -11,19 +11,19 @@ class Solution {
         // Code here
         vector<int>bfs;
         vector<int>vis(V,0);
-               queue<int>q;
-               vis[0]=1;
-               q.push(0);
-               while(!q.empty()){
-                   int node=q.front();
-                   q.pop();
-                   bfs.push_back(node);
-                   for(auto it:adj[node]){
-                       if(!vis[it]){
-                           q.push(it);
-                           vis[it]=1;
-                       }
+        queue<int>q;
+        vis[0]=1;
+        q.push(0);
+        while(!q.empty()){
+          int node=q.front();
+          q.pop();
+          bfs.push_back(node);
+           for(auto it:adj[node]){
+                if(!vis[it]){
+                    q.push(it);
+                    vis[it]=1;
                    }
+            }
  
         }
         return bfs;
