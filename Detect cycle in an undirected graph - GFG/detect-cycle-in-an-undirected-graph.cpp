17c7d5163186e996c540160b19d91d3141cb5110
Ankit Kumar <61880496+anktsverma@users.noreply.github.com>
2022-02-19 11:45:08 +0530

    Added solution - LeetHub

--- Detect cycle in an undirected graph - GFG/detect-cycle-in-an-undirected-graph.cpp
+++ Detect cycle in an undirected graph - GFG/detect-cycle-in-an-undirected-graph.cpp
@@ -6,22 +6,37 @@ using namespace std;
 class Solution {
   public:
     // Function to detect cycle in an undirected graph.
-    bool cyclebfs(int s,int V,vector<int>adj[],vector<int>&vis){
-        queue<pair<int,int>>q;
-        q.push({s,-1});
+    
+    // bool cyclebfs(int s,int V,vector<int>adj[],vector<int>&vis){
+    //     queue<pair<int,int>>q;
+    //     q.push({s,-1});
+    //     vis[s]=1;
+    //     while(!q.empty()){
+    //         int node=q.front().first;
+    //         int par=q.front().second;
+    //         q.pop();
+    //         for(auto it:adj[node]){
+    //             if(!vis[it]){
+    //                 vis[it]=1;
+    //                 q.push({it,node});
+    //             }
+    //             else if(it!=par){
+    //                 return true;
+    //             }
+    //         }
+    //     }
+    //     return false;
+    // }
+    
+    bool cycledfs(int s,int prev,int V,vector<int>adj[],vector<int>&vis){
         vis[s]=1;
-        while(!q.empty()){
-            int node=q.front().first;
-            int par=q.front().second;
-            q.pop();
-            for(auto it:adj[node]){
-                if(!vis[it]){
-                    vis[it]=1;
-                    q.push({it,node});
-                }
-                else if(it!=par){
-                    return true;
-                }
+        for(auto it:adj[s]){
+            if(!vis[it]){
+              vis[it]=1;
+              if(cycledfs(it,s,V,adj,vis)) return true;
+            }
+            else if(it!=prev){
+                return true;
             }
         }
         return false;
@@ -31,7 +46,7 @@ class Solution {
         vector<int>vis(V,0);
         for(int i=0;i<V;i++){
             if(!vis[i]){
-                if(cyclebfs(i,V,adj,vis)){
+                if(cycledfs(i,-1,V,adj,vis)){
                     return true;
                 }
             }
