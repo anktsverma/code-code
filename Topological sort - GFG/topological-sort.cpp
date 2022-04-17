e8cb85d3ad86fdc47560fcb2e67d261ba115a15a
Ankit Kumar <61880496+anktsverma@users.noreply.github.com>
2022-04-17 07:25:57 +0530

    Added solution - LeetHub

--- Topological sort - GFG/topological-sort.cpp
+++ Topological sort - GFG/topological-sort.cpp
@@ -7,37 +7,67 @@ class Solution
 {
 	public:
 	//Function to return list containing vertices in Topological order. 
-	void topoSorting(int node,vector<int>adj[],stack<int>&st,vector<int>&vis){
-	    vis[node]=1;
+// 	void topoSorting(int node,vector<int>adj[],stack<int>&st,vector<int>&vis){
+// 	    vis[node]=1;
 	    
-	    for(auto it:adj[node]){
-	        if(!vis[it]){
-	            topoSorting(it,adj,st,vis);
-	        }
-	    }
-	    st.push(node);
-	}
+// 	    for(auto it:adj[node]){
+// 	        if(!vis[it]){
+// 	            topoSorting(it,adj,st,vis);
+// 	        }
+// 	    }
+// 	    st.push(node);
+// 	}
 	
-	vector<int> topoSort(int V, vector<int> adj[]) 
-	{
-	    // code here
-	    stack<int>st;
-	    vector<int>vis(V,0);
-	    for(int i=0;i<V;i++){
-	        if(!vis[i]){
-	            topoSorting(i,adj,st,vis);
-	        }
-	    }
+// 	vector<int> topoSort(int V, vector<int> adj[]) 
+// 	{
+// 	    // code here
+// 	    stack<int>st;
+// 	    vector<int>vis(V,0);
+// 	    for(int i=0;i<V;i++){
+// 	        if(!vis[i]){
+// 	            topoSorting(i,adj,st,vis);
+// 	        }
+// 	    }
 	    
-	    vector<int>topo;
+// 	    vector<int>topo;
 	    
-	    while(!st.empty()){
-	        int val=st.top();
-	        st.pop();
-	        topo.push_back(val);
-	    }
-	    return topo;
-	}
+// 	    while(!st.empty()){
+// 	        int val=st.top();
+// 	        st.pop();
+// 	        topo.push_back(val);
+// 	    }
+// 	    return topo;
+// 	}
+
+ 	vector<int> topoSort(int V, vector<int> adj[]) {
+ 	    vector<int>indegree(V,0);
+ 	    queue<int>q;
+ 	    for(int i=0;i<V;i++){
+ 	        for(auto it:adj[i]){
+ 	            indegree[it]++;
+ 	        }
+ 	    }
+ 	    
+ 	    for(int i=0;i<V;i++){
+ 	        if(indegree[i]==0){
+ 	            q.push(i);
+ 	        }
+ 	    }
+ 	    vector<int>topo;
+ 	    while(!q.empty()){
+ 	        int node =q.front();
+ 	        q.pop();
+ 	        topo.push_back(node);
+ 	        
+ 	        for(auto it:adj[node]){
+ 	            indegree[it]--;
+ 	            if(indegree[it]==0){
+ 	                q.push(it);
+ 	            }
+ 	        }
+ 	    }
+ 	    return topo;
+ 	}
 };
 
 // { Driver Code Starts.
