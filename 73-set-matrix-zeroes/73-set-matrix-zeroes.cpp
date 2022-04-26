4f72f87016fca9895f5d3e068c940c442c801e78
Ankit Kumar <61880496+anktsverma@users.noreply.github.com>
2022-04-26 11:47:35 +0530

    Time: 19 ms (52.96%), Space: 13.2 MB (82.49%) - LeetHub

--- 73-set-matrix-zeroes/73-set-matrix-zeroes.cpp
+++ 73-set-matrix-zeroes/73-set-matrix-zeroes.cpp
@@ -1,22 +1,49 @@
 class Solution {
 public:
-    void setZeroes(vector<vector<int>>& matrix) {
-        set<int>r,c;
-        for(int i=0;i<matrix.size();i++){
-            for(int j=0;j<matrix[i].size();j++){
-                if(matrix[i][j]==0){
-                    c.insert(j);
-                    r.insert(i);
-                }
-            }
-        }
+//     void setZeroes(vector<vector<int>>& matrix) {
+//         set<int>r,c;
+//         for(int i=0;i<matrix.size();i++){
+//             for(int j=0;j<matrix[i].size();j++){
+//                 if(matrix[i][j]==0){
+//                     c.insert(j);
+//                     r.insert(i);
+//                 }
+//             }
+//         }
         
-      for(int i=0;i<matrix.size();i++){
-          for(int j=0;j<matrix[i].size();j++){
-              if(c.find(j)!=c.end() || r.find(i)!=r.end()){
-                  matrix[i][j]=0;
+//       for(int i=0;i<matrix.size();i++){
+//           for(int j=0;j<matrix[i].size();j++){
+//               if(c.find(j)!=c.end() || r.find(i)!=r.end()){
+//                   matrix[i][j]=0;
+//               }
+//           }
+//       }
+//     }
+    
+    void setZeroes(vector<vector<int>>& matrix) {
+          int n=matrix.size();
+          int m=matrix[0].size();
+          vector<int>r(n,-1),c(m,-1);
+        
+          for(int i=0;i<n;i++){
+              for(int j=0;j<m;j++){
+                  if(matrix[i][j]==0){
+                      r[i]=0;
+                      c[j]=0;
+                  }
               }
           }
-      }
+        
+          for(int i=0;i<n;i++){
+              for(int j=0;j<m;j++){
+                   if(r[i]==0){
+                       matrix[i][j]=0;
+                   }
+                   else if(c[j]==0){
+                       matrix[i][j]=0;
+                   }
+              }  
+          }
     }
+    
 };
