c638d2d9e375cae5e7cc3e303039755f3d95f479
Ankit Kumar <61880496+anktsverma@users.noreply.github.com>
2022-06-02 20:24:19 +0530

    Time: 2 ms (58.63%), Space: 8.5 MB (16.89%) - LeetHub

--- 94-binary-tree-inorder-traversal/94-binary-tree-inorder-traversal.cpp
+++ 94-binary-tree-inorder-traversal/94-binary-tree-inorder-traversal.cpp
@@ -21,6 +21,7 @@ public:
     // }
     
     vector<int> inorderTraversal(TreeNode* root) {
+        if(!root) return {};
         vector<int>ans;
         stack<TreeNode*> st;
         TreeNode *node=root;
