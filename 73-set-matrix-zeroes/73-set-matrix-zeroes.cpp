62672de49152c94f6dfe4128f86ba19a7c61fef1
Ankit Kumar <61880496+anktsverma@users.noreply.github.com>
2022-06-15 09:27:58 +0530

    Time: 23 ms (40.58%), Space: 13.3 MB (18.64%) - LeetHub

--- 73-set-matrix-zeroes/73-set-matrix-zeroes.cpp
+++ 73-set-matrix-zeroes/73-set-matrix-zeroes.cpp
@@ -22,28 +22,27 @@ public:
 //     }
     
 //  TC - O(N*M)   SC- O(N+M)
-    void setZeroes(vector<vector<int>>& matrix) {
-          int n=matrix.size();
-          int m=matrix[0].size();
-          vector<int>r(n,-1),c(m,-1);
+    void setZeroes(vector<vector<int>>& mat) {
+        int m=mat.size();
+        int n=mat[0].size();
+        vector<int> r(m,-1),c(n,-1);
         
-          for(int i=0;i<n;i++){
-              for(int j=0;j<m;j++){
-                  if(matrix[i][j]==0){
-                      r[i]=0;
-                      c[j]=0;
-                  }
-              }
-          }
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(mat[i][j]==0){
+                    r[i]=0;
+                    c[j]=0;
+                }
+            }
+        }
         
-          for(int i=0;i<n;i++){
-              for(int j=0;j<m;j++){
-                   if(r[i]==0 || c[j]==0){
-                       matrix[i][j]=0;
-                   }
-
-              }  
-          }
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(r[i]==0 || c[j]==0){
+                    mat[i][j]=0;
+                }
+            }
+        }
     }
     
 };
