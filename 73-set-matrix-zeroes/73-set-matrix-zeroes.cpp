4c9ae7aad8ebc33355034b4f1ac7bb7743a81421
Ankit Kumar <61880496+anktsverma@users.noreply.github.com>
2022-07-13 11:32:45 +0530

    Time: 29 ms (25.61%), Space: 13.1 MB (81.69%) - LeetHub

--- 73-set-matrix-zeroes/73-set-matrix-zeroes.cpp
+++ 73-set-matrix-zeroes/73-set-matrix-zeroes.cpp
@@ -1,35 +1,13 @@
 class Solution {
 public:
-//   TC - O(N*M*(log(N)+log(M))
-//     void setZeroes(vector<vector<int>>& matrix) {
-//         set<int>r,c;
-//         for(int i=0;i<matrix.size();i++){
-//             for(int j=0;j<matrix[i].size();j++){
-//                 if(matrix[i][j]==0){
-//                     c.insert(j);
-//                     r.insert(i);
-//                 }
-//             }
-//         }
-        
-//       for(int i=0;i<matrix.size();i++){
-//           for(int j=0;j<matrix[i].size();j++){
-//               if(c.find(j)!=c.end() || r.find(i)!=r.end()){
-//                   matrix[i][j]=0;
-//               }
-//           }
-//       }
-//     }
-    
-//  TC - O(N*M)   SC- O(N+M)
-    void setZeroes(vector<vector<int>>& mat) {
-        int m=mat.size();
-        int n=mat[0].size();
-        vector<int> r(m,-1),c(n,-1);
-        
+    void setZeroes(vector<vector<int>>& matrix) {
+        int m=matrix.size();
+        int n=matrix[0].size();
+        vector<int>r(m,-1);
+        vector<int>c(n,-1);
         for(int i=0;i<m;i++){
             for(int j=0;j<n;j++){
-                if(mat[i][j]==0){
+                if(matrix[i][j]==0){
                     r[i]=0;
                     c[j]=0;
                 }
@@ -39,10 +17,10 @@ public:
         for(int i=0;i<m;i++){
             for(int j=0;j<n;j++){
                 if(r[i]==0 || c[j]==0){
-                    mat[i][j]=0;
+                    matrix[i][j]=0;
                 }
             }
         }
+        
     }
-    
 };
