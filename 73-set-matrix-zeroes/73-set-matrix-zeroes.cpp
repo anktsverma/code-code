42988cda3743de9ac7cd25cb7fae1afca967901e
Ankit Kumar <61880496+anktsverma@users.noreply.github.com>
2022-08-13 13:58:42 +0530

    Time: 22 ms (56.80%), Space: 13.3 MB (52.88%) - LeetHub

--- 73-set-matrix-zeroes/73-set-matrix-zeroes.cpp
+++ 73-set-matrix-zeroes/73-set-matrix-zeroes.cpp
@@ -4,6 +4,7 @@ public:
         bool col0=false;
         int m=matrix.size();
         int n=matrix[0].size();
+        
         for(int i=0;i<m;i++){
             if(matrix[i][0]==0) col0=true;
             for(int j=1;j<n;j++){
@@ -18,13 +19,14 @@ public:
             for(int j=n-1;j>=1;j--){
                 if(matrix[0][j]==0 || matrix[i][0]==0) matrix[i][j]=0;
             }
+            if(col0) matrix[i][0]=0;
         }
         
-        if(col0){
-            for(int i=0;i<m;i++){
-                matrix[i][0]=0;
-            }
-        }
+        // if(col0){
+        //     for(int i=0;i<m;i++){
+        //         matrix[i][0]=0;
+        //     }
+        // }
         
     }
 };
